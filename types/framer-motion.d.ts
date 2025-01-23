c2cbf74d2099bda88014196e107ab694d552d475
mcdendrick <115506627+mcdendrick@users.noreply.github.com>
2025-01-22 23:20:13 -0800

    fix and set up framer-motion types

--- types/framer-motion.d.ts
+++ types/framer-motion.d.ts
@@ -1,3 +1,11 @@
+import { 
+  motion as Motion,
+  AnimatePresence as AP,
+  MotionProps,
+} from 'framer-motion/types';
+
 declare module 'framer-motion' {
-    export * from 'framer-motion/types'
-  }
+  export const motion: typeof Motion;
+  export const AnimatePresence: typeof AP;
+  export type { MotionProps };
+}
